33736a6a59e49b94396b62422dd70a48c8e61f06
Codemaico <devwithmii@gmail.com>
2024-09-18 15:08:58 +0200

    Routing for pages set, UI work done on home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink,RouterLinkActive, CommonModule, RouterOutlet, HomeComponent, ProjectsComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectsComponent } from './projects/projects.component';
 
+
 export const routes: Routes = [
-   { path: "", redirectTo: "home", pathMatch: 'full'},
+   { path: "", redirectTo: "/home", pathMatch: 'full'},
    { path: "home", component: HomeComponent},
    { path: "about-me", component: AboutMeComponent},
    { path: "projects", component: ProjectsComponent}
 ];
+
